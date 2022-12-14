e676269e0dfd6821e5234bb55eb96ae8b880523c
4thdraw <4thdraw2@gmail.com>
2022-12-14 17:58:27 +0900

    로고 수정 및 스와이퍼수정

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import 'swiper/css/navigation';
 import 'swiper/css/pagination';
 import 'swiper/css/scrollbar';
 
+
 function App() {
   return (
     <div className="App">
@@ -16,7 +17,7 @@ function App() {
         </h1>
         <Navi></Navi>        
       </header>
-      <Sliderswiper></Sliderswiper>
+      <Worksswiper></Worksswiper>
       <Preinterview></Preinterview>
       <Footer></Footer>
     </div>
@@ -52,34 +53,30 @@ function Navi(){
   )
 }
 
-const Sliderswiper = () => {
- 
-    return (
+const Worksswiper = () => {
+    return(
       <Swiper
-      // install Swiper modules
       modules={[Navigation, Pagination, Scrollbar, A11y, Autoplay]}
-      spaceBetween={50}
-      slidesPerView={3}
+      spaceBetween={0}
+      slidesPerView={1}
       navigation
       autoplay={{
-        "delay": 2500,
-        "disableOnInteraction": false
+        delay: 2500,
+        disableOnInteraction: false,
       }}
       pagination={{ clickable: true }}
       scrollbar={{ draggable: true }}
-      onSwiper={(swiper) => console.log(swiper)}
       onSlideChange={() => console.log('slide change')}
-
-      
-    >
-        <SwiperSlide>Slide 1</SwiperSlide>
-        <SwiperSlide>Slide 2</SwiperSlide>
-        <SwiperSlide>Slide 3</SwiperSlide>
-        <SwiperSlide>Slide 4</SwiperSlide>     
+      onSwiper={(swiper) => console.log(swiper)}
+       >
+          <SwiperSlide>Slide 1</SwiperSlide>
+          <SwiperSlide>Slide 2</SwiperSlide>
+          <SwiperSlide>Slide 3</SwiperSlide>
+          <SwiperSlide>Slide 4</SwiperSlide>     
       </Swiper>
-    );
- 
+    )
 }
+
 const Preinterview = () => {
   return(
     <section className="section py-5 text-center">
