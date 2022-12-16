d6c768e1d0713e84467be624ab1cbf44b6ed54f3
4thdraw <4thdraw2@gmail.com>
2022-12-16 12:24:17 +0900

    props DB흐름 세팅하기

--- src/App.js
+++ src/App.js
@@ -13,9 +13,9 @@ function App() {
     <div className="App" id="top">
       <Header></Header>
       <Worksswiper></Worksswiper>
-      <Preinterview contentid="portfolio"></Preinterview>      
-      <Preinterview contentid="me"></Preinterview>
-      <Preinterview contentid="preInterview"></Preinterview>
+      <Preinterview contentid="portfolio" title="포트폴리오" num='0'></Preinterview>      
+      <Preinterview contentid="me"  title="공지사항" num='1'></Preinterview>
+      <Preinterview contentid="preInterview" title="사전인터뷰" num='0'></Preinterview>
       <Footer></Footer>
     </div>
   );
@@ -75,25 +75,37 @@ const Worksswiper = () => {
 }
 
 const Preinterview = (props) => {
-  const interviewjson = [{
+  const interviewjson = [ [
+    {
     subject : '코딩보다 이게 더 어려워',
-    content : '일단 만나봅시다! 사장'
-  },
-  {
-    subject : '사진인터뷰의 용도를 잘 이해하고 기입하셔야합니다.',
-    content : `그들이 나에게 요구하는것이 나한테 있는지 여부파악하고 잘 써야합니다.
-    없는 거는 없다 있는거는 있다. 
-    나만 나중에 피곤한 일이 없도록 써야합니다.`
-  }]
-
+    content : '일단 만나봅시다! 사장',   
+    },
+    {
+      subject : '사진인터뷰의 용도를 잘 이해하고 기입하셔야합니다.',
+      content : `그들이 나에게 요구하는것이 나한테 있는지 여부파악하고 잘 써야합니다.
+      없는 거는 없다 있는거는 있다. 
+      나만 나중에 피곤한 일이 없도록 써야합니다.`
+    }    
+  ],
+  [
+    {
+    subject : '공지사항제목',
+    content : '공지사항내용',   
+    },
+    {
+      subject : '공지사항알림',
+      content : `엔터치고 싶어서 그냥 빽틱으로 표현함`
+    }    
+  ]
+]
   return(
     
     <section className="section py-5 text-center" id={props.contentid}>
-        <h3>규칙위에 틀을 깨는 개발자가 되겠습니다.</h3>
+        <h3>{props.title}</h3>
         <div className="py-5 container-md text-start">
           <ul>
              {
-              interviewjson.map(function(value, index){
+              interviewjson[props.num].map(function(value, index){
                   //this조심할것
                   return(
                     <li  key={'interview'+index}>
